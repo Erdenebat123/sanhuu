f36e35292cb90201d53dd94be2c80ce0ce4f1e0e
Эрдэнэ Бат <erka20426@gmail.com>
2025-02-07 13:52:43 +0800

    event listeneruudiig uusgelee

---
+++ app.js
@@ -0,0 +1,23 @@
+// delgesttei ajillah controller
+var uiController = (function(){})();
+
+// sanhuutei ajillah controller
+var financeController = (function(){})();
+
+//programiig holboh controller
+var appController = (function(uiController,financeController){
+
+var ctrlAddItem = function(){
+    console.log("hi ");
+}
+
+document.querySelector('.add__btn').addEventListener('click',function(){
+    ctrlAddItem();
+})
+document.addEventListener('keypress',function(event){
+    if(event.keyCode === 13 || event.which === 13){
+       ctrlAddItem();
+    }
+})
+
+})(uiController,financeController)
